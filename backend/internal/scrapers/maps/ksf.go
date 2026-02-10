62bff4ecb1e356adf27502a5abf3fdd1489f8926
Miguel Correa <miguel.correa@unesp.br>
2026-02-10 20:55:34 -0300

    Added KSF maps scraper

---
+++ backend/internal/scrapers/maps/ksf.go
@@ -0,0 +1,91 @@
+package maps
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
+	"time"
+)
+
+type KSFScraper struct {
+	baseURL string
+	client  *http.Client
+}
+
+func NewKSFScraper(baseURL string) *KSFScraper {
+	return &KSFScraper{
+		baseURL: baseURL,
+		client: &http.Client{
+			Timeout: 10 * time.Second,
+		},
+	}
+}
+
+type Map struct {
+	MapID    int    `json:"mapID"`
+	Name     string `json:"name"`
+	Tier     int    `json:"tier"`
+	Created  int    `json:"created"`
+	Playtime int    `json:"playtime"`
+	BCount   int    `json:"b_count"`
+	IsLinear bool   `json:"isLinear"`
+}
+
+type MapsResponse struct {
+	Maps []Map `json:"maps"`
+}
+
+func (s *KSFScraper) FetchMaps(path string) ([]Map, error) {
+	body, err := os.ReadFile(path)
+
+	var response MapsResponse
+	if err != nil {
+		resp, err := s.client.Get(s.baseURL)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			return nil, errors.New("unexpected status code")
+		}
+
+		body, err = io.ReadAll(resp.Body)
+		if err != nil {
+			return response.Maps, err
+		}
+	}
+	html := string(body)
+
+	// Find where maps array is - pattern from JS: /({\\"maps\\":.*?})]\\n/
+	re := regexp.MustCompile(`({\\"maps\\":.*?})]\\n`)
+	matchResult := re.FindStringSubmatch(html)
+
+	if len(matchResult) < 2 {
+		return nil, errors.New("maps array not found")
+	}
+
+	jsonStr := matchResult[1]
+	// Remove all backslashes
+	jsonFixed := strings.ReplaceAll(jsonStr, `\`, ``)
+
+	if err := json.Unmarshal([]byte(jsonFixed), &response); err != nil {
+		return nil, fmt.Errorf("failed to parse maps JSON: %w", err)
+	}
+
+	fmt.Printf("Found %d maps\n", len(response.Maps))
+
+	for i, m := range response.Maps {
+		println(m.Tier)
+		if i > 2 {
+			break
+		}
+	}
+
+	return response.Maps, err
+}
