f638ea987367f589c0c55d9f6fb1630f7bc9ff1a
Miguel Correa <miguel.correa@unesp.br>
2026-02-11 19:10:52 -0300

    Included added to SurfMap interface

--- frontend/src/types/Map.ts
+++ frontend/src/types/Map.ts
@@ -2,7 +2,7 @@ export interface SurfMap {
     id: number;
     name: string;
     tier: number;
-    year: number;
+    added: number;
     completions: number;
     playtime_seconds: number;
     comp_per_hour: number;
