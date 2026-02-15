ab585c3829b2557fd08cd4db385b60276274c944
Miguel Correa <miguel.correa@unesp.br>
2026-02-15 14:17:58 -0300

    Updated FetchMapCompletionsFromPlayerRecord to also return MapID

--- backend/internal/scrapers/players/ksf.go
+++ backend/internal/scrapers/players/ksf.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -32,35 +33,39 @@ type completionsResponse struct {
 
 // Gets the total number of completions via the player record API.
 // Used when scraping the maps, as the completions are not included in the maps page.
-func (s *KSFScraper) FetchMapCompletionsFromPlayerRecord(steamID string, mapName string) (int, error) {
+// Returns mapID, completions, err
+func (s *KSFScraper) FetchMapCompletionsFromPlayerRecord(steamID string, mapName string) (int, int, error) {
 
 	url := s.baseURL + "/" + steamID + "/records/map/" + mapName + "?game=css&mode=0"
 	resp, err := s.client.Get(url)
 	if err != nil {
-		return -1, err
+		return -1, -1, err
 	}
 	defer func() { _ = resp.Body.Close() }()
 
 	if resp.StatusCode != http.StatusOK {
-		return -1, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+		return -1, -1, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
 	}
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return -1, err
+		return -1, -1, err
 	}
 
 	var response completionsResponse
 
 	if err := json.Unmarshal(body, &response); err != nil {
-		return -1, fmt.Errorf("failed to parse map records JSON: %w", err)
+		return -1, -1, fmt.Errorf("failed to parse map records JSON: %w", err)
 	}
-
+	mapID, err := strconv.Atoi(response.MapID)
+	if err != nil {
+		return -1, -1, fmt.Errorf("failed parsing mapID to int: %v", err)
+	}
 	for _, zone := range response.Zones {
 		if zone.ZoneID == 0 {
-			return zone.TotalRanks, nil
+			return mapID, zone.TotalRanks, nil
 		}
 	}
 
-	return -1, fmt.Errorf("failed to find completions for given map")
+	return -1, -1, fmt.Errorf("failed to find completions for given map")
 }
